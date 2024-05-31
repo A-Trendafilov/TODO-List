d320951cff586f947ca44e17697558a0f0881a23
Angel Trendafilov <aatrendafilov@gmail.com>
2024-05-31 13:45:23 +0300

    add necessary files

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
