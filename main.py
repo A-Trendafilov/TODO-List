b944924fa48ba936ea936e7064245275f7e8913a
Angel Trendafilov <aatrendafilov@gmail.com>
2024-06-04 15:42:26 +0300

    add create account func and html template.

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask, render_template, flash, redirect, url_for, request
-from flask_bootstrap import Bootstrap
-from flask_login import login_user, LoginManager
+from flask_bootstrap import Bootstrap5
+from flask_login import (
+    login_user,
+    LoginManager,
+    current_user,
+    login_required,
+    logout_user,
+)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from db_model import db, User
@@ -16,7 +22,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 db.init_app(app)
-Bootstrap(app)
+Bootstrap5(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -32,60 +38,65 @@ def load_user(user_id):
 #     db.create_all()
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if request.form["action"] == "login":
-            result = db.session.execute(
-                db.select(User).where(User.email == form.email.data)
-            )
-            user = result.scalar()
-            if not user:
-                flash("That email does not exist, please try again.")
-                return redirect(url_for("login"))
-            elif not check_password_hash(user.password, form.password.data):
-                flash("Password incorrect, please try again.")
-                return redirect(url_for("login"))
-            else:
-                login_user(user)
-                return redirect(url_for("get_all_posts"))
-        elif request.form["action"] == "register":
-            return redirect(url_for("create_account"))
-
-    return render_template(
-        "index.html",
-        form=form,
-    )  # current_user=current_user)
-
-
-@app.route("/crate-account")
-def create_account():
-    form = RegisterForm()
-    if form.validate_on_submit():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
         result = db.session.execute(
-            db.select(User).where(User.email == form.email.data)
+            db.select(User).where(User.email == login_form.email.data)
+        )
+        user = result.scalar()
+        if not user:
+            flash("That email does not exist, please try again.", "danger")
+            return redirect(url_for("home"))
+        elif not check_password_hash(user.password, login_form.password.data):
+            flash("Password incorrect, please try again.", "error")
+            return redirect(url_for("home"))
+        else:
+            login_user(user)
+            return redirect(url_for("todos"))
+    return render_template("index.html", form=login_form, current_user=current_user)
+
+
+@app.route("/create-account", methods=["GET", "POST"])
+def create_account():
+    reg_form = RegisterForm()
+    if reg_form.validate_on_submit():
+        result = db.session.execute(
+            db.select(User).where(User.email == reg_form.email.data)
         )
         user = result.scalar()
         if user:
-            flash("You've already signed up with that email, log in instead!")
+            flash("You've already signed up with that email, log in instead!", "error")
             return redirect(url_for("home"))
 
         hash_and_salted_password = generate_password_hash(
-            form.password.data,
+            reg_form.password.data,
             method="pbkdf2:sha256",
             salt_length=8,
         )
         new_user = User(
-            email=form.email.data,
+            email=reg_form.email.data,
             password=hash_and_salted_password,
-            name=form.name.data,
+            name=reg_form.name.data,
         )
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
-        return redirect(url_for("get_all_posts"))
-    return render_template("register.html", form=form, current_user=current_user)
+        flash("Account created successfully! You are now logged in.", "success")
+        return redirect(url_for("todos"))
+    return render_template("register.html", form=reg_form, current_user=current_user)
+
+
+@app.route("/todos")
+def todos():
+    return render_template("todos.html")
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
