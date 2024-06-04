661233a19a22ee0cf26ccc111e22d59977b702d4
Angel Trendafilov <aatrendafilov@gmail.com>
2024-06-04 09:46:09 +0300

    make the db model.

--- db_model.py
+++ db_model.py
@@ -1,5 +1,8 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
+from datetime import datetime
 
 
 class Base(DeclarativeBase):
@@ -9,5 +12,21 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(100))
+    todos = relationship("TODO", back_populates="author")
+
+
 class TODO(db.Model):
-    pass
+    __tablename__ = "todos"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(String(200))
+    due_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    completed: Mapped[bool] = mapped_column(Boolean, default=False)
+    author_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    author = relationship("User", back_populates="todos")
